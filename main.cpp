3405e53de518cc6fc89868ce0849ed834846c8af
akbarazy <muh.akbar.alfarizy@gmail.com>
2026-03-24 09:55:59 +0700

    Implement basic render for snake body

--- main.cpp
+++ main.cpp
@@ -1,12 +1,42 @@
 #include "raylib.h"
+#include <vector>
+
+struct Position {
+    int x;
+    int y;
+};
 
 int main() {
-    InitWindow(800, 600, "Snake Game");
+    std::vector<Position> snake = {
+        {5, 3},
+        {5, 2},
+        {5, 1},
+        {4, 1},
+        {3, 1},
+        {2, 1},
+        {2, 2},
+        {1, 2}
+    };
+    int cellSize = 20, 
+        screenWidth = 800, screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "Snake Game");
     SetTargetFPS(60);
 
     while(!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(GRAY);
+        
+        for(int i = 0; i < snake.size(); i++) {
+            int light = 250 - i * 25;
+            DrawRectangle(
+                snake[i].x * cellSize, 
+                snake[i].y * cellSize, 
+                cellSize, cellSize, 
+                {0, 121, 241, (unsigned char)(int)light}
+            );
+        }
+
         EndDrawing();
     }
 
